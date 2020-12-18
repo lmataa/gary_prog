65df5a90065c292c12d559a242b8f9d56bcf93a3
saragary <sara@gary.es>
2020-12-18 17:53:08 +0100

    ejercicio 5 creado

---
+++ ex_5.cpp
@@ -0,0 +1,30 @@
+////crear un programa de porcentaje de variacion.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+
+
+//mis variables 
+
+int inicial, final, vp;
+
+
+//lo que tengo que pedir para poder calcularlo
+
+
+cout << "Dime el precio inicial: "; 
+cin>>inicial;
+cout << " Dime el precio final: ";
+cin>>final;
+
+vp = 100 *(final-inicial)/inicial;
+
+
+cout<< "la variacion porcentual es de: "<<vp;
+//como hago para que salte a otra linea al final?
+
+
+}
