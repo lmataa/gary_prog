52971640ee7cbf42b6905552188c7083e159bc74
saragary <sara@gary.es>
2020-12-18 16:50:28 +0100

    ejercicio 4 modificado

--- ex_4.cpp
+++ ex_4.cpp
@@ -1,7 +1,26 @@
 /* Aquí iría un enunciado
- *
- *
- *
+ *Construir un programa que calculado a partir de la anterior
+fórmula.
+Recuerde que la función arco-seno está implementada en la función  de la bilioteca           .
+La salida debe ser
+Finalidad: Ejemplo básico de asignación a una variable del resultado de una expresión.
+Dificultad Baja PI/6=ARCOSENO (0.5)
+ 
 */
 
 #include<iostream>
+#include<cmath>
+using namespace std;
+ int main (){
+
+double pi;
+ 
+pi= 6 * asin * 0.5;
+
+cout << "PI vale:  " <<pi;
+
+
+//NO SE PORQUE ME SALE ERROR?
+
+ }
+
